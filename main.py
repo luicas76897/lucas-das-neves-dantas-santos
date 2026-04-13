8bc619add33976b647268f39db40e18c4d3f71d5
Your Name <you@example.com>
2026-04-13 20:18:29 -0300

    meu primeiro codigo

---
+++ main.py
@@ -0,0 +1,11 @@
+print("welo word")
+
+variavel_1 = input("qual seu nome? ")
+print(variavel_1)
+
+if variavel_1 == "lucas":
+    print("seu nome esta correto")
+elif variavel_1 == "neves":
+    print("o sobrenome esta correto")
+else:
+    print('não é o seu nome')
