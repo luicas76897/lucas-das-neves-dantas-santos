7c44e069121aafb6fb68b810123b2a58dada94c6
Your Name <you@example.com>
2026-04-29 20:53:43 -0300

    atividade 29,04,2026

---
+++ cadastro .py
@@ -0,0 +1,25 @@
+
+
+while True:
+    senha=input("digite sua senha:")
+
+    for letra in list(senha):
+        try:
+            int(letra)
+
+        except ValueError:
+            print("digite sua senhn")
+            print("tente denvo/a")
+            break
+
+         while True:
+         valida = True
+         if input("senha esta correta"):
+         invalida = False
+         if valida("senha valida"):
+
+
+        
+            
+     
+
